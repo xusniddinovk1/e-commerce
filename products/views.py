669ec42bd8f8caa6818045e2f36e1073d7f22575
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-09 21:07:54 +0500

    initial commit

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from products.filters import ProductFilter
 from products.models import Category, Review, Product
@@ -29,6 +30,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
+
+    permission_classes = [IsAuthenticated]
 
     pagination_class = CustomPagination  # api/products/?page=2
 
