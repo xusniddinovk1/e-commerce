efe35e153cc36dd16ee62dcb3e4947bb4c7e4921
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-09 20:46:25 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/auth/token/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/auth/token/verify', TokenVerifyView.as_view(), name='token_verify'),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schem_view.without_ui(cache_timeout=0), name='schem-json'),
     path('', schem_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
