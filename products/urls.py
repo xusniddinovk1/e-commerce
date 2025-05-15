51d4d233bc7d584e7fa01867d289779f2fdde31e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-15 11:02:09 +0500

    initial commit

--- products/urls.py
+++ products/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .services import admin_replenish_stock
-from .views import ProductViewSet, ReviewViewSet, CategoryViewSet
+from .views import ProductViewSet, ReviewViewSet, CategoryViewSet, OrderViewSet
 from .services.product_view_history import ProductViewHistoryCreate
 from .services.flash_sale import check_flash_sale, FlashSaleListCreateView
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
