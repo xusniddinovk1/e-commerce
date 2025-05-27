ed05b11dda3510518c78a3d7bbed8eb082256266
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-27 14:53:23 +0500

    update test.py

--- products/test/test_category.py
+++ products/test/test_category.py
@@ -2,7 +2,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from products.models import Category
-from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
